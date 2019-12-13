fe48dffa5de112d9ce84b2acd73f68ea11f9d937
Amar Catovic <amarzenica@gmail.com>
2019-12-13 18:40:18 +0100

    Improvements

--- Assets/scripts/weather.js
+++ Assets/scripts/weather.js
@@ -1,6 +1,7 @@
 var key = "c918647878d1f020d5c226f15183e169";
 var jedinica = "metric";
 var searchParametar = "q";
+var logoVremena = document.getElementById("VrijemeDiv");
 
 function pretraziVrijeme(Grad) {
   fetch(
@@ -13,7 +14,7 @@ function pretraziVrijeme(Grad) {
       init(rezultat);
     })
     .catch(function(err) {
-      console.log("Nepoznat grad" + err);
+      console.log("Nepoznat grad " + err);
     });
 }
 
@@ -31,18 +32,11 @@ function init(rezultatServer) {
 
   console.log(rezultatServer);
 
-  var logoVremena = document.getElementById("VrijemeDiv");
-  var slika = document.createElement("img");
-  var paragraf = document.createElement("p");
-  var video = document.createElement("iframe");
-  video.setAttribute("id", "musicFrame");
   var muzika = document.createElement("audio");
   muzika.setAttribute("id", "myAudio");
   var src = document.createElement("source");
   src.setAttribute("id", "backgroundSrc");
   src.setAttribute("type", "audio/mpeg");
-  video.setAttribute("width", "500");
-  video.setAttribute("height", "300");
 
   document.body.appendChild(muzika);
 
@@ -117,10 +111,8 @@ function init(rezultatServer) {
       break;
   }
 
-  logoVremena.appendChild(video);
-  logoVremena.appendChild(paragraf);
-  logoVremena.appendChild(slika);
   document.body.appendChild(logoVremena);
+  popuniPodatke(rezultatServer);
 }
 
 document.querySelector("#searchBtn").addEventListener("click", () => {
@@ -146,3 +138,45 @@ document.querySelector("#searchBtnNav").addEventListener("click", () => {
     pretraziVrijeme(trazi);
   }
 });
+
+function popuniPodatke(rezultatServer) {
+  logoVremena.style.display = "flex";
+
+  document.querySelector("#searchDiv").style.height = "0vh";
+  document.querySelector("#searchDiv").style.display = "none";
+  var mjestoPretrage = document.querySelector("#weatherIn");
+  var opisVremena = document.querySelector("#opisVremena");
+  mjestoPretrage.innerHTML = `Weather in ${rezultatServer.name}, ${rezultatServer.sys.country}`;
+  opisVremena.innerHTML = rezultatServer.weather[0].description;
+
+  var stepeni = document.querySelector("#stepeniP");
+  var stepeniC = Math.round(rezultatServer.main.temp - 273.15);
+  stepeni.innerHTML = stepeniC + "°C";
+
+  var vrijemeLogo = document.querySelector("#weatherLogo");
+  vrijemeLogo.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${rezultatServer.weather[0].icon}@2x.png`
+  );
+
+  var windSpeed = document.querySelector("#windP");
+  var windDir = document.querySelector("#windDirP");
+  windSpeed.innerHTML = rezultatServer.wind.speed + " m/s";
+  windDir.innerHTML = rezultatServer.wind.deg + "°";
+
+  var pritisak = document.querySelector("#pritisakP");
+  pritisak.innerHTML = rezultatServer.main.pressure + "hPa";
+
+  var sunrise = document.querySelector("#sunriseP");
+  var sunset = document.querySelector("#sunsetP");
+  sunrise.innerHTML = new Date(
+    (rezultatServer.sys.sunrise + rezultatServer.timezone) * 1000
+  )
+    .toISOString()
+    .substr(11, 5);
+  sunset.innerHTML = new Date(
+    (rezultatServer.sys.sunset + rezultatServer.timezone) * 1000
+  )
+    .toISOString()
+    .substr(11, 5);
+}
